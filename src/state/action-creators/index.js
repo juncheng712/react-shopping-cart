3508c82b8a783e9dd87e4890e83591bbda20431e
juncheng <wongjun_cheng@outlook.com>
2021-06-29 13:42:29 +0800

    add redux to filtered components

--- src/state/action-creators/index.js
+++ src/state/action-creators/index.js
@@ -4,7 +4,49 @@ export const fetchProducts = () => {
         const res  = await fetch('/api/products');
         const data = await res.json();
         dispatch({
-            type: 'FETCH_PRODUCTS', payload: data
+            type: 'FETCH_PRODUCTS', 
+            payload: data
+        })
+    }
+}
+
+export const filterBySize = (products, size) => {
+    return (dispatch) => {
+        dispatch({
+            type: 'FILTER_BY_SIZE',
+            payload: {
+                size: size,
+                items: 
+                (size === "") 
+                ? 
+                products 
+                : 
+                products.filter(product => product.availableSizes.indexOf(size)>=0)
+            }
+        })
+    }
+}
+
+export const sortProducts = (filteredProducts, sort) => {
+    const sortedProducts = filteredProducts.slice();
+    if (sort === "latest") {
+        sortedProducts.sort((a, b) => (a._id > b._id ? 1 : -1));
+    } else {
+        sortedProducts.sort((a, b) => (
+            sort === "lowest" 
+            ?
+            a.price > b.price ? 1 : -1   // lowest price
+            :
+            a.price > b.price ? -1 : 1   // highest price
+        ))
+    }
+    return (dispatch) => {
+        dispatch({
+            type: 'ORDER_PRODUCTS_BY_PRICE', 
+            payload: {
+                sort: sort,
+                items: sortedProducts
+            }
         })
     }
 }
